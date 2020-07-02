2a66a7d4011fd1321ba0f12a897948826f6a3db5
Jayon Kim <jpkim921@gmail.com>
2020-07-02 00:25:58 -0400

    create articles and graph component and add to [id].js

--- pages/currency/[id].js
+++ pages/currency/[id].js
@@ -1,16 +1,53 @@
-import DataChart from "../../components/datachart"
+import Head from 'next/head'
+import DataChart from "../../components/datachart";
+import Graph from "../../components/graph";
+import Articles from "../../components/articles";
+
+const container = {
+  maxWidth: "55rem",
+  padding: "0 1rem",
+  margin: "3rem auto 6rem",
+}
+
+const gridLayout = {
+  display: "grid",
+  gridTemplateColumns: "2fr 1fr",
+  gridTemplateRows: "2fr 1fr",
+  gap: "15px 10px",
+  justifyItems: "start",
+};
+
+const graphBox = {
+  gridColumnStart: "1",
+  gridRowStart: "1"
+}
+const dataChartBox = {
+  gridColumnStart: "2",
+  gridRowStart: "1",
+};
+const articleBox = {
+  gridColumnStart: "1",
+  gridRowStart: "2",
+};
 
 export default ({ data }) => {
-  console.log(data)
+  console.log(data);
   return (
     <>
+      <Head>
+        <title>{data[0].id}</title>
+      </Head>
+      <div className="container" style={container}>
       <h1>Currency page</h1>
-      <DataChart data={data} />
-      
+        <div className="gridLayout" style={gridLayout}>
+          <Graph style={graphBox} />
+          <DataChart data={data} style={dataChartBox} />
+          <Articles style={articleBox} />
+        </div>
+      </div>
     </>
   );
 };
-
 
 export async function getServerSideProps(context) {
   // Fetch data from external API
